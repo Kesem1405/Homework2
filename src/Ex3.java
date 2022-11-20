9b0a348d1c41844a639372a04b4ae3039aad07d3
Kesem Halis <Kesem1212@gmail.com>
2022-11-20 09:37:45 +0200

    Updated

--- src/Ex3.java
+++ src/Ex3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex3 {
+public class Ex3{
     public static void main(String[] args) {
         int userNumber;
         Scanner scanner = new Scanner(System.in);
@@ -8,31 +8,33 @@ public class Ex3 {
             System.out.println("Please enter a odd number that bigger than 3.");
             userNumber = scanner.nextInt();
         } while (userNumber < 3 || userNumber % 2 == 0);
-        printDiamonds(userNumber);
+        printDiamond(userNumber);
     }
-        public static void printDiamonds(int number){
-            int j, i, space, k;
-            int index = 1;
-            space = number / 2;  //5/2=2
-            for (i = 1; i <= number; i+=2) {
-                System.out.println(" ");
-                for (j = 0; j < index; j++) {
-                    System.out.print("*");
-                }
-                for (k = 0; k < space; k++) {
-                    System.out.print(" ");
-                }
-                if (index != number) {
-                    index = index + 2;
-                }
+
+    public static void printDiamond(int number) {
+        int i, j, k, space;
+        space = number / 2;
+        for( i = 1; i<=number;i+=2) {
+            for( j = 0; j< space; j++){
+                System.out.print(" ");
             }
-            index = index - 2;
-            for (i = 0; i < space; i++) {
-                System.out.println(" ");
-                for (k = 0; k < index; k++) {
-                    System.out.print("*");
-                }
-                index = index - 2;
+            for(k = 0; k< i;k++){
+                System.out.print("*");
             }
+            space = space-1;
+            System.out.println(" ");
+
+        }
+        space = 0;
+        for (i = number -2;i>=1;i-=2){
+            space = space + 1;
+            for (j = 0; j<space; j++){
+                System.out.print(" ");
+            }
+            for(k = 0; k<i; k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
     }
+}
